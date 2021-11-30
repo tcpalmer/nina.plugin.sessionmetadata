d4a90e0e7044badd28f37d296003c473d7e1b6c3
Tom Palmer <palmer.tom@gmail.com>
2021-11-30 09:48:40 -0500

    partial work on controlling numeric output

--- NINA.Plugin.SessionMetaData/NINA.Plugin.SessionMetaData/Utility.cs
+++ NINA.Plugin.SessionMetaData/NINA.Plugin.SessionMetaData/Utility.cs
@@ -34,5 +34,19 @@ namespace SessionMetaData.NINAPlugin.Utility {
             // Perform substitutions and replace any remaining spaces
             return p.GetImageFileString(fileName).Replace(' ', '_');
         }
+
+        /// <summary>
+        /// Format a double
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns>formatted</returns>
+        public static string FormatDouble(Double value) {
+            // TODO: not even sure this makes sense ...
+            //   You could change all the member fields to strings but then the output would record them as strings, not doubles which is bad
+            //   Otherwise, you'd have to get in front of the CSV and JSON serialization - might be possible:
+            //     https://joshclose.github.io/CsvHelper/examples/configuration/class-maps/type-conversion/
+            //     https://www.newtonsoft.com/json/help/html/T_Newtonsoft_Json_JsonConverterAttribute.htm
+            return String.Format("{0:0.####}", value);
+        }
     }
 }
--- NINA.Plugin.SessionMetaData/PluginTest/UtilityTest.cs
+++ NINA.Plugin.SessionMetaData/PluginTest/UtilityTest.cs
@@ -8,7 +8,7 @@ namespace SessionMetaData.NINAPlugin.Test {
     public class UtilityTest {
 
         [Test]
-        public void TestIt() {
+        public void TestFileNameTokenSubstitution() {
 
             string result = Utility.Utility.FileNameTokenSubstitution(null, GenImageMetaData("My Target Name"));
             result.Should().BeNull();
@@ -45,6 +45,24 @@ namespace SessionMetaData.NINAPlugin.Test {
 
             result = Utility.Utility.FileNameTokenSubstitution("AcquisitionDetails:|<>\"/\\?*-$$TARGETNAME$$", GenImageMetaData(new DateTime(2021, 6, 1, 12, 32, 30), "My Target Name"));
             result.Should().Be("AcquisitionDetails-My_Target_Name");
+        }
+
+        [Test]
+        public void TestFormatDouble() {
+            string result = Utility.Utility.FormatDouble(1.2345678);
+            result.Should().Be("1.2346");
+
+            result = Utility.Utility.FormatDouble(1.23);
+            result.Should().Be("1.23");
+
+            result = Utility.Utility.FormatDouble(0.0);
+            result.Should().Be("0");
+
+            result = Utility.Utility.FormatDouble(367.87451480006246);
+            result.Should().Be("367.8745");
+
+            result = Utility.Utility.FormatDouble(0.4510157391181826);
+            result.Should().Be("0.451");
         }
 
         private ImageMetaData GenImageMetaData(string targetName) {
